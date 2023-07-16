8328cd1a0558c18398b2206cdd1401d326ac952d
Mustafa Dikyar <mustafa.dikyar@setur.com.tr>
2023-07-16 19:29:08 +0300

    Models was added.

---
+++ elasticsearch-nest-api/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace elasticsearch_nest_api.Models;
+
+public class Product
+{
+    [PropertyName("_id")]
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public ProductDetail? Detail { get; set; }
+}
---
+++ elasticsearch-nest-api/Models/ProductDetail.cs
@@ -0,0 +1,7 @@
+﻿namespace elasticsearch_nest_api.Models;
+
+public class ProductDetail
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
